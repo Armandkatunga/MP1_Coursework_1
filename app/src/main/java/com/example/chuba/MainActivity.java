8e73fba02f7da2be7ee2d2b7ab4642d122b6a73d
armand katunga <katungaarmand1@gmail.com>
2019-04-23 23:55:31 +0200

    Main Activity (class, UI)
    Project structure re-factory
    Initial methods created: ini(), events(), notify(String msg), switchToActivity().
    
    UI : template is ready with bottom navigation buttons : on click of a button a fragment will load into a FrameLayout tmp

--- app/src/main/java/com/example/chuba/MainActivity.java
+++ app/src/main/java/com/example/chuba/MainActivity.java
@@ -2,12 +2,65 @@ package com.example.chuba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button home_btn,map_btn,about_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ini();
+        events();
+
+    }
+
+   // initialization | attachment
+    private void ini() {
+
+        home_btn = findViewById(R.id.home_btn);
+        map_btn = findViewById(R.id.map_btn);
+        about_btn = findViewById(R.id.about_btn);
+
+    }
+    // event binding
+    private void events() {
+
+        home_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        map_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        about_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+
+    // Notifcatin center
+    private void notify(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+    // Redirection
+    private  void switchToActivity(){
+
     }
 }
